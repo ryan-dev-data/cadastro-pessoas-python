a33b0424ec7a77a2ad84da2396bdb392c73b486d
Ryan Rebouças <ryanfr2303@gmail.com>
2026-01-22 19:24:04 -0300

    Enhance person registration with age statistics
    Added percentage calculations for age groups and updated output messages.

--- Cadastro de Pessoas (semana 3).py
+++ Cadastro de Pessoas (semana 3).py
@@ -1,33 +1,46 @@
-print("=====CADASTRO DE PESSOAS=====")
+print("=====CADASTRO DE PESSOAS=====\n")
 
-quantidade = int(input("Quantas pessoas serão cadastradas? "))
+quantidade = int(input("Quantas pessoas serão cadastradas: "))
 
 nomes = []
 idades = []
 
 for i in range(quantidade):
-    nome = str(input("Digite o nome: "))
-    idade = int(input("Digite a idade: "))
 
+    nome = input("Digite o nome da pessoa: ")
+    idade = int(input("Digite a idade da pessoa: "))
     nomes.append(nome)
     idades.append(idade)
+    maiores = 0
+    menores = 0
 
-maiores = 0
-menores = 0
 
-print("=====CONDIÇÕES DE ENTRADA=====\n")
+
+print("\n=====CONDIÇÃO DE ENTRADA=====")
 for i in range(len(nomes)):
-     if idades[i]>=18:
-        print(nomes[i], "-", idades[i], "anos - Acesso liberado")
+    if idades[i] >=18:
+        print(nomes[i], "-", idades[i], "anos - Maior de idade")
         maiores += 1
-     else:
-        print(nomes[i], "-", idades[i], "anos - Acesso negado")
+    else:
+        print(nomes[i], "-", idades[i], "anos - Menor de idade")
         menores += 1
 
+
+
 print("\n=====CONTAGEM DE PESSOAS=====")
-print("Total de pessoas: ", len(nomes))
-print("Maiores de idade: ", maiores)
-print("Menores de idade: ", menores)
+print("Quantidade de pessoas :", len(nomes))
+total = len(nomes)
+perc_maiores = (maiores/total)*100
+perc_menores = (menores/total)*100
+print(f"Maiores de idade:  {maiores} ({perc_maiores:.1f}%)")
+print(f"Menores de idade:  {menores} ({perc_menores:.1f}%)")
+
+print("\n=====PÚBLICO MAJORITÁRIO=====")
+if perc_maiores>50:
+    print("\nPublico majoriatariamente maior de idade")
+else:
+    print("\nPublico majoriatariamente menor de idade")
+
 
 
 
